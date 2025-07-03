cc7e40c623abb41e1137c2cd36a6a516ef8e28b2
Weverson_Rodrigues <weversonwrs8@gmail.com>
2025-07-03 14:02:22 -0300

    config api veiculo

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 # iniciar a FASTAPI
 app = FastAPI()
 
-
+# Area responsavel pela criação dos modelos da fastapi da classe Motorista
 @app.get("/motoristas/", response_model=list[schemas.Motorista])
 async def get_all_motoristas(db: Session = Depends(get_db)):
     """
@@ -51,3 +51,48 @@ async def delete_motorista(motorista_id: int, db: Session = Depends(get_db)):
         return db_deleted_motorista
     raise HTTPException(status_code=404, detail="Motorista não encontrado")
     
+
+# Area responsavel pela criação dos modelos da fastapi da classe Veiculo
+@app.get("/veiculos/", response_model=list[schemas.Veiculo])
+async def get_all_veiculos(db: Session = Depends(get_db)):
+    """
+    Retorna todos os veículos cadastrados.
+    """
+    return services.get_veiculos(db)
+
+@app.get("/veiculos/{veiculo_id}", response_model=schemas.Veiculo)
+async def get_veiculo_by_id(veiculo_id: int, db: Session = Depends(get_db)):
+    """
+    Retorna um veículo específico pelo ID.
+    """
+    veiculo_queryset = services.get_veiculo(db, veiculo_id)
+    if veiculo_queryset:
+        return veiculo_queryset
+    raise HTTPException(status_code=404, detail="Veículo não encontrado")
+
+@app.post("/veiculos/", response_model=schemas.Veiculo)
+async def create_new_veiculo(veiculo: schemas.VeiculoCreate, db: Session = Depends(get_db)):
+    """
+    Cria um novo veículo.
+    """
+    return services.create_veiculo(db, veiculo)
+
+@app.put("/veiculos/{veiculo_id}", response_model=schemas.Veiculo)
+async def update_veiculo(veiculo_id: int, veiculo: schemas.VeiculoCreate, db: Session = Depends(get_db)):
+    """
+    Atualiza um veículo existente.
+    """
+    db_updated_veiculo = services.update_veiculo(db, veiculo, veiculo_id)
+    if not db_updated_veiculo:
+        raise HTTPException(status_code=404, detail="Veículo não encontrado")
+    return db_updated_veiculo
+
+@app.delete("/veiculos/{veiculo_id}", response_model=schemas.Veiculo)
+async def delete_veiculo(veiculo_id: int, db: Session = Depends(get_db)):
+    """
+    Deleta um veículo pelo ID.
+    """
+    db_deleted_veiculo = services.delete_veiculo(db, veiculo_id)
+    if db_deleted_veiculo:
+        return db_deleted_veiculo
+    raise HTTPException(status_code=404, detail="Veículo não encontrado")
